b7d6727a418b4d31e41a742ac3c883d346b5814e
CinthyaJG9 <cjimenezg2000@alumno.ipn.mx>
2022-10-19 09:25:13 -0500

    Updates

---
+++ precache-manifest.9d7c9eae8350ab845afa26cddbf6d296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "311140fc09cebcb531ff",
+    "url": "CinthyaJG9.github.io/recetas-chido/static/css/main.53fe3408.chunk.css"
+  },
+  {
+    "revision": "311140fc09cebcb531ff",
+    "url": "CinthyaJG9.github.io/recetas-chido/static/js/main.311140fc.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "CinthyaJG9.github.io/recetas-chido/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "ad455d792a6cb131b635",
+    "url": "CinthyaJG9.github.io/recetas-chido/static/js/2.ad455d79.chunk.js"
+  },
+  {
+    "revision": "f6ca51a353020aabebfa",
+    "url": "CinthyaJG9.github.io/recetas-chido/static/js/runtime~main.f6ca51a3.js"
+  },
+  {
+    "revision": "1835f1070895fd70b90e333e9d9048c9",
+    "url": "CinthyaJG9.github.io/recetas-chido/index.html"
+  }
+];
